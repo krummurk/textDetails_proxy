5e3eb85ba3b62c932d529dd90d410f4ae2a00bef
hsinyi.wc.uma@gmail.com <Github@312>
2019-05-28 21:46:43 -0700

    Implement proxy server

--- server/index.js
+++ server/index.js
@@ -12,15 +12,35 @@ app.use(express.static(path.join(__dirname, '../client')));
 
 
 app.get('/restaurant/:name', function(req, res) {
+    console.log('umamiiiii')
     res.header("X-Content-Type", "text/javascript");
     res.sendFile(path.join(__dirname, '/../client/index.html'))
 });
 
 app.get('/API/restaurant/:name', function(req, res) {
+    console.log('uma')
     res.header("X-Content-Type", "text/javascript");
     var q = req.params.name;
     res.redirect('http://localhost:3003/API/restaurant/'+q)
 });
+
+
+app.get('/reservations/:restID/bookedTimes', function(req, res) {
+    console.log('Roger')
+    res.header("X-Content-Type", "text/javascript");
+    var q = req.params.restID;
+    res.redirect('http://localhost:3010/reservations/' + q + '/bookedTimes');
+});
+
+
+app.get('/reservations/:restID/reservations', function(req, res) {
+    console.log('Roger2')
+
+    res.header("X-Content-Type", "text/javascript");
+    var q = req.params.restID;
+    res.redirect('http://localhost:3010/reservations/' +  '/reservations');
+});
+
 
 
 app.listen(3000, function() {
